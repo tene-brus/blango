9d0d21f527cb9a57a762686590470e33e453ed98
Themis Nikolis <themis629@gmail.com>
2024-02-12 20:52:24 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -58,15 +58,24 @@ class Dev(Configuration):
       'django.contrib.contenttypes',
       'django.contrib.sessions',
       'django.contrib.messages',
+      'django.contrib.sites',
       'django.contrib.staticfiles',
       'blango_auth',
       'blog',
       'crispy_forms',
       'crispy_bootstrap5',
       'debug_toolbar',
+      "allauth", 
+      "allauth.account", 
+      "allauth.socialaccount", 
+      "allauth.socialaccount.providers.google",
   ]
   ADMINS = [("T N", "themis.nik@outlook.com")]
-
+  SITE_ID = 1
+  ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+  ACCOUNT_EMAIL_REQUIRED = True
+  ACCOUNT_USERNAME_REQUIRED = False
+  ACCOUNT_AUTHENTICATION_METHOD = "email"
   
   LOGGING = {
     "version": 1,
--- blango/urls.py
+++ blango/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path("ip/", blog.views.get_ip),
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/profile/", blango_auth.views.profile, name="profile"),
+    path("accounts/", include("allauth.urls")),
 ]
 
 if settings.DEBUG:
