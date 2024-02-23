c8afef071881adf1719cdbe71b046c2346c92351
Themis Nikolis <themis629@gmail.com>
2024-02-23 18:05:51 +0000

    Finish JavaScript classes

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,27 +1,24 @@
-for(let i = 0; i < 10; i += 1) {
-  console.log('for loop i: ' + i)
+class Greeter {
+  constructor (name) {
+    this.name = name
+  }
+
+  getGreeting () {
+    if (this.name === undefined) {
+      return 'Hello, no name'
+    }
+
+    return 'Hello, ' + this.name
+  }
+
+  showGreeting (greetingMessage) {
+    console.log(greetingMessage)
+  }
+
+  greet () {
+    this.showGreeting(this.getGreeting())
+  }
 }
 
-let j = 0
-while(j < 10) {
-  console.log('while loop j: ' + j)
-  j += 1
-}
-
-let k = 10
-
-do {
-  console.log('do while k: ' + k)
-} while(k < 10)
-
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-numbers.forEach((value => {
-  console.log('For each value ' + value)
-}))
-
-const doubled = numbers.map(value => value * 2)
-
-console.log('Here are the doubled numbers')
-
-console.log(doubled)
+const g = new Greeter('Patchy')  // Put your name here if you like
+g.greet()
