d2bb2f2c0d3a4e2d5fd3140bd561e965174c8609
Themis Nikolis <themis629@gmail.com>
2024-02-23 18:18:08 +0000

    Finish JavaScript promises

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,24 +1,25 @@
-class Greeter {
-  constructor (name) {
-    this.name = name
-  }
-
-  getGreeting () {
-    if (this.name === undefined) {
-      return 'Hello, no name'
-    }
-
-    return 'Hello, ' + this.name
-  }
-
-  showGreeting (greetingMessage) {
-    console.log(greetingMessage)
-  }
-
-  greet () {
-    this.showGreeting(this.getGreeting())
-  }
+function resolvedCallback(data) {
+  console.log('Resolved with data ' +  data)
 }
 
-const g = new Greeter('Patchy')  // Put your name here if you like
-g.greet()
+function rejectedCallback(message) {
+  console.log('Rejected with message ' + message)
+}
+
+const lazyAdd = function (a, b) {
+  const doAdd = (resolve, reject) => {
+    if (typeof a !== "number" || typeof b !== "number") {
+      reject("a and b must both be numbers")
+    } else {
+      const sum = a + b
+      resolve(sum)
+    }
+  }
+
+  return new Promise(doAdd)
+}
+
+const p = lazyAdd(3, 4)
+p.then(resolvedCallback, rejectedCallback)
+
+lazyAdd("nan", "alsonan").then(resolvedCallback, rejectedCallback)
